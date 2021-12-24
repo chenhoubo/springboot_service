bf1021f2712e394562a9f5ef315ca62441691e4d
Harris <chenhoubo123>
2021-12-24 20:42:58 +0800

    小问题

--- doc/mysql.sql
+++ doc/mysql.sql
@@ -11,3 +11,16 @@ CREATE TABLE `user` (
   `end_time` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10001 DEFAULT CHARSET=utf8;
+
+--模拟数据
+INSERT INTO user
+(name, status, is_deleted, json, create_by, update_by, create_time, update_time, end_time)
+VALUES('张三', 0, 0, '{
+  "age": "25",
+  "tel": "123456789090",
+  "style": "哈哈",
+  "gender": "男",
+  "address": "广东省深圳市",
+  "username": "admin",
+  "password": "123456"
+}', 'default', 'default', 1639990424276, 1639990424276, 0);
